0e9b9a7bd2233ce6d393058e1a5d2d4671c5db16
kmsarikasingh <gplsarikasingh@gmail.com>
2023-11-13 00:20:31 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+
+import "./App.css"
+
+import PropCointainer from "./components/PropCointainer";
+
+
+function App() {
+
+  
+ 
+  
+  return (
+    <div style={{maxWidth:"1000px",margin:"auto",minHeight:"100vh"}} >
+
+     <PropCointainer />   
+    </div>
+  );
+}
+
+export default App;
